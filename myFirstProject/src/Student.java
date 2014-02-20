d50858765c47db07e46c096e04658b2afe6df888
jaBear <hq16@yandex.ru>
2014-02-20 20:10:57 +0400

    Hello

---
+++ myFirstProject/src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+
+    public String name = "N/A";
+
+    public int age = 15;
+
+    public void sayHello() {
+        System.out.println("Hello");
+        System.out.println("My name is: " + name + "My age is: " + age);
+
+    };
+}
